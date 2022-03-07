f29397a02f041b9a96ad937089d0ebbeda73a93f
gerard singian <gerard.singian@outlook.com>
2022-03-07 10:24:19 +0800

    Add project files.

---
+++ PosPrint/BusinessSetup.xaml.cs
@@ -0,0 +1,50 @@
+﻿using PosPrint.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PosPrint
+{
+    /// <summary>
+    /// Interaction logic for BusinessSetup.xaml
+    /// </summary>
+    public partial class BusinessSetup : Window
+    {
+        public BusinessSetup()
+        {
+            InitializeComponent();
+            txtBUsinessName.Text = "FRESH OPTIONS MEXICO";
+            txtPhone.Text = "23232323";
+            txtLocation.Text = "MEXICO PAMPANGA";
+            txtBranch.Text = "MEXICO";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Business.Businesses.Add(new Business
+            {
+ 
+                BusinessName = txtBUsinessName.Text,
+                Branch = txtBranch.Text,
+                Location= txtLocation.Text,
+                Phone = txtPhone.Text
+            
+            });
+
+            //MessageBox.Show("Business Setup Successfully");
+            this.Hide();
+            var main = new MainWindow();
+            main.Show();
+        }
+    }
+}
